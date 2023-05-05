0a8d0486f3c1537d5d7afd96309767198834e6ef
extendo0 <npapanika@gmail.com>
2023-05-05 12:30:47 +0600

    1

---
+++ Shop/Shop/Views/AddCarPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Shop.Views;
+
+public partial class AddCarPage : ContentPage
+{
+    int count = 0;
+    public AddCarPage()
+	{
+		InitializeComponent();
+	}
+    private void Car(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            NewCar.Text = $"Clicked {count} time";
+        else
+            NewCar.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(NewCar.Text);
+    }
+}
