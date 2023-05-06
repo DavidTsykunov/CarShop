f9c9bd9b6ad6c8f9783631670ea7db655e0ab4fe
DavidTsykunov <davidTsykunov@gmail.com>
2023-05-06 14:06:31 +0600

    Добавил базу данных
    Firebase database added+

--- Shop/Shop/Views/ProfilePage.xaml.cs
+++ Shop/Shop/Views/ProfilePage.xaml.cs
@@ -1,9 +1,35 @@
+using Firebase.Database;
+using Firebase.Database.Query;
+using Shop.Model;
+using System.Collections.ObjectModel;
+
 namespace Shop.Views;
 
 public partial class ProfilePage : ContentPage
 {
-	public ProfilePage()
+    FirebaseClient firebaseClient = new FirebaseClient("https://car-shop-fde53-default-rtdb.europe-west1.firebasedatabase.app/");
+    public ObservableCollection<TodoItem> TodoItems { get; set; } = new ObservableCollection<TodoItem>();
+    public ProfilePage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = this;
+
+        var collection = firebaseClient
+        .Child("Todo")
+        .AsObservable<TodoItem>()
+        .Subscribe((item) =>
+        {
+            if (item.Object != null)
+            {
+                TodoItems.Add(item.Object);
+            }
+        });
+    }
+    private void OnBtnClciked(object sender, EventArgs e)
+    {
+        firebaseClient.Child("Todo").PostAsync(new TodoItem
+        {
+            Title = TitleEntry.Text,
+        });
+    }
 }
